e58055eb610dccf320a2b22a063f4e4504481ffa
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-05-07 22:10:49 -0300

    feat: adding doom emacs, starship, wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require "wezterm"
+
+local config = {
+	color_scheme = "nord",
+	font = wezterm.font("Monaco Nerd Font"),
+	font_size = 13,
+	window_decorations = "NONE"
+}
+
+return config
