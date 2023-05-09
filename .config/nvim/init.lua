a5c69a7fa884a9ebcf02092bde42174dfa080601
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-05-09 19:24:07 -0300

    feat: updated nvim and wezterm configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
+require "autocmds"
 require "keys"
 require "options"
 require "plugins"
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,9 @@
 local wezterm = require "wezterm"
 
 local config = {
-	color_scheme = "nord",
+	color_scheme = "vscode-dark",
+  color_scheme_dirs = { "/home/hrq/.config/wezterm/colorschemes" },
+  enable_tab_bar = false,
 	font = wezterm.font("Monaco Nerd Font"),
 	font_size = 13,
 	window_decorations = "NONE"
